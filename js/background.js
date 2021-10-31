3b2a4b8d497f63d8b3131ea2c29286377e4a2924
nathan <keller6@hotmail.fr>
2021-10-31 18:52:10 +0100

    v1

---
+++ js/background.js
@@ -0,0 +1,21 @@
+let myDiv = document.querySelector('.iss');
+let back  = document.querySelector('body')
+myDiv.addEventListener('mouseover', () => {
+    back.style.backgroundImage = `url('https://cdn.futura-sciences.com/buildsv6/images/wide1920/5/4/4/544bab25a3_50169469_iss-20-ans.jpg')`;
+
+});
+myDiv.addEventListener('mouseleave', () => {
+    back.style.backgroundImage = null;
+
+});
+
+let myDiv2 = document.querySelector('.blob');
+let back2  = document.querySelector('body')
+myDiv2.addEventListener('mouseover', () => {
+    back2.style.backgroundImage = `url('https://cdn.radiofrance.fr/s3/cruiser-production/2021/08/0d77cb0c-e619-4e6e-b990-19f60c0971d8/801x410_blob.jpg')`;
+
+});
+myDiv2.addEventListener('mouseleave', () => {
+    back2.style.backgroundImage = null;
+
+});
